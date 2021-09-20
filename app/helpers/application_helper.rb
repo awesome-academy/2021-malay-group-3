0a0818eeff523c17c5c18fc6d2760471917e64d6
Khadijah <khadijah99.mdaud@gmail.com>
2021-09-20 18:01:27 +0800

    signup/register

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,5 @@ module ApplicationHelper
   def full_title page_title = ""
     base_title = "Course Registration System"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
-end
+  end
 end
