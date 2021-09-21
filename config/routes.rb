d659af45cd68917de2f7a069e15263a8b25113c5
Iskandar <walhamdulillah158@gmail.com>
2021-09-21 11:58:31 +0800

    staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/help', as: "help"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
